ada3aeb028e793e5af916bcc4f5a7f0fd1fd5f03
KOGNON <sephorakognon125@gmail.com>
2025-12-21 23:41:04 +0100

    Create TracePassage model for entry/exit tracking
    Added TracePassage model to track entry/exit history.

--- parking_app/models.py
+++ parking_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class TracePassage(models.Model):
+    """Stocke l'historique des entrées/sorties pour les statistiques"""
+    immatriculation = models.CharField(max_length=20)
+    date_entree = models.DateTimeField(auto_now_add=True)
+    date_sortie = models.DateTimeField(null=True, blank=True)
+    type_service = models.CharField(max_length=50, default='Aucun')
+
+    def __str__(self):
+        return f"{self.immatriculation} ({self.date_entree})"
