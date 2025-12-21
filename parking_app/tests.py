6a2ea31aee46f14b95b476f1af7bf32b7365253b
KOGNON <sephorakognon125@gmail.com>
2025-12-21 23:42:28 +0100

    Implement tests for TracePassage model
    Add unit tests for TracePassage model functionality.

--- parking_app/tests.py
+++ parking_app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import TracePassage
 
-# Create your tests here.
+class StatistiquesTest(TestCase):
+    def setUp(self):
+        # On crée des données de test
+        TracePassage.objects.create(immatriculation="AA-123-BB", type_service="LIVRAISON")
+        TracePassage.objects.create(immatriculation="CC-456-DD", type_service="ENTRETIEN")
+
+    def test_calcul_frequentation(self):
+        """Vérifie que le nombre total de voitures est correct"""
+        nb_voitures = TracePassage.objects.count()
+        self.assertEqual(nb_voitures, 2)
